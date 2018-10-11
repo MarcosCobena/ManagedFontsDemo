0fc225dcf99c02b3a630d378c7ecd4abc4ff1ff2
Marcos Cobeña Morián <marcoscm.digital@gmail.com>
2018-10-11 17:35:31 +0200

    Initial commit :-)

---
+++ FontRasterizer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using SixLabors.Fonts;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.Primitives;
+using SixLabors.Shapes;
+
+namespace FontsDemo
+{
+    public static class FontRasterizer
+    {
+        private const int MinimumSide = 1;
+
+        public static void RenderText(
+            Font font, string text, string outputPath, Rgba32? backgroundColor = null)
+        {
+            if (font == null)
+            {
+                return;
+            }
+
+            var style = new RendererOptions(font, 72);
+            var rawShapes = TextBuilder.GenerateGlyphs(text, new PointF(0, 0), style);
+            var shapes = rawShapes.Translate(-rawShapes.Bounds.X, -rawShapes.Bounds.Y);
+
+            const int safetyFrame = 0;
+            var bounds = shapes.Bounds;
+            var width = (int)Math.Ceiling(bounds.Width + safetyFrame);
+            var height = (int)Math.Ceiling(bounds.Height + safetyFrame);
+
+            if (width <= 0)
+            {
+                width = MinimumSide;
+            }
+
+            if (height <= 0)
+            {
+                height = MinimumSide;
+            }
+
+            using (var render = new Image<Rgba32>(width, height))
+            {
+                var actualBackgroundColor = backgroundColor ?? Rgba32.Transparent;
+                render.Mutate(operation => operation.Fill(actualBackgroundColor));
+                render.Mutate(operation => operation.Fill(Rgba32.Black, shapes));
+
+                if (File.Exists(outputPath))
+                {
+                    File.Delete(outputPath);
+                }
+
+                using (var stream = File.Create(outputPath))
+                {
+                    render.SaveAsPng(stream);
+                }
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,150 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media.Imaging;
+using Avalonia.Threading;
+using SixLabors.Fonts;
+using SixLabors.Fonts.Exceptions;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace FontsDemo
+{
+    public class MainWindow : Window
+    {
+        private const int DefaultFontSize = 128;
+        private const string GlyphsDirectoryName = "Glyphs";
+        private const string RenderFileName = "Render.png";
+
+        private readonly string _asciiChars;
+        private readonly string _workingDirectory;
+        private readonly string _renderOutputPath;
+
+        private DropDown _fontsDropDown;
+        private ListBox _glyphsListBox;
+        private TextBox _inputTextBox;
+        private Image _outputImage;
+        private Font _font;
+
+        public MainWindow()
+        {
+            var location = Assembly.GetEntryAssembly().Location;
+            _workingDirectory = Path.GetDirectoryName(location);
+            _renderOutputPath = Path.Combine(_workingDirectory, RenderFileName);
+
+            InitializeComponent();
+            InitializeControls();
+
+            _asciiChars = GetASCIIChars();
+            _inputTextBox.Text = "Wave Engine :-)";
+            PopulateFonts();
+        }
+
+        private void InitializeControls()
+        {
+            _fontsDropDown = this.FindControl<DropDown>("FontsDropDown");
+            _fontsDropDown.SelectionChanged += UpdateFont;
+
+            _glyphsListBox = this.FindControl<ListBox>("GlyphsListBox");
+
+            _inputTextBox = this.FindControl<TextBox>("InputTextBox");
+            _inputTextBox.TextInput += UpdateRenderImage;
+
+            _outputImage = this.FindControl<Image>("OutputImage");
+        }
+
+        private void UpdateFont(object sender, SelectionChangedEventArgs e)
+        {
+            var dropDown = sender as DropDown;
+            var fontFamily = dropDown.SelectedItem as FontFamily;
+            _font = fontFamily.CreateFont(DefaultFontSize);
+
+            PopulateGlyphs();
+            UpdateRenderImage(_inputTextBox, null);
+        }
+
+        private void PopulateFonts()
+        {
+            Task.Run(() =>
+            {
+                var fonts = new FontCollection();
+
+                var fontPaths = Directory.EnumerateFiles("/Library/Fonts", "*.ttf");
+
+                foreach (var item in fontPaths)
+                {
+                    try
+                    {
+                        var fontFamily = fonts.Install(item);
+                    }
+                    catch (InvalidFontFileException)
+                    {
+                        Trace.WriteLine($"Ops, I couldn't load {item}");
+                    }
+                }
+
+                Dispatcher.UIThread.InvokeAsync(() =>
+                {
+                    _fontsDropDown.Items = fonts.Families;
+                    var firstFontFamily = fonts.Families.FirstOrDefault();
+                    _fontsDropDown.SelectedItem = firstFontFamily;
+                });
+            });
+        }
+
+        private void PopulateGlyphs()
+        {
+            Task.Run(() =>
+            {
+                var outputDirectory = Path.Combine(_workingDirectory, GlyphsDirectoryName);
+
+                if (Directory.Exists(outputDirectory))
+                {
+                    Directory.Delete(outputDirectory, true);
+                }
+
+                Directory.CreateDirectory(outputDirectory);
+
+                var glyphs = new List<object>();
+
+                foreach (var item in _asciiChars)
+                {
+                    var safeFileName = $"{(byte)item}.png";
+                    var outputPath = Path.Combine(outputDirectory, safeFileName);
+                    FontRasterizer.RenderText(_font, item.ToString(), outputPath, Rgba32.White);
+                    var bitmap = new Bitmap(outputPath);
+                    var glyph = Tuple.Create(item, bitmap);
+                    glyphs.Add(glyph);
+                }
+
+                Dispatcher.UIThread.InvokeAsync(() => _glyphsListBox.Items = glyphs);
+            });
+        }
+
+        private void UpdateRenderImage(object sender, EventArgs e)
+        {
+            var textBox = sender as TextBox;
+            var text = textBox.Text;
+
+            Task.Run(() =>
+            {
+                FontRasterizer.RenderText(_font, text, _renderOutputPath);
+
+                Dispatcher.UIThread.InvokeAsync(() => _outputImage.Source = new Bitmap(_renderOutputPath));
+            });
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        private static string GetASCIIChars() => 
+            string.Concat(Enumerable.Range(0, 256).Select(index => (char)index));
+    }
+}
